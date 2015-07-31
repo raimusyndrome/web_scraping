90d3dbdc72fccdf29fc5c4e4540967ddd022ca82
murai <murai@localhost.localdomain>
2015-07-31 16:15:06 +0900

    テストのひな形を追加

---
+++ gatherer/test/gatherer_test.rb
@@ -0,0 +1,17 @@
+
+require "test/unit"
+
+class TestGatherer < Test::Unit::TestCase
+    sub_test_case "Set list page test" do
+        setup do
+            # セットのページを読み込む
+        end
+    end
+
+    sub_test_case "Card detail page test" do
+        setup do
+            # カードのページを読み込む
+        end
+    end
+end
+
