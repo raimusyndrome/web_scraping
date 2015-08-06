80d088031ce0d24569af5398e46df55a593894ba
murai <murai@localhost.localdomain>
2015-08-06 16:50:31 +0900

    フレーバー一覧の取得処理を追加

--- gatherer/gatherer_scraping.rb
+++ gatherer/gatherer_scraping.rb
@@ -1,7 +1,30 @@
 # -*- encoding: utf-8 -*-
 
+require "csv"
 require "gatherer_parser"
+require "../utils/nokogiri_utils"
 
 def get_flavour_file(set_name)
+    # フレーバーテキスト一覧を取得する。
+    page_url = get_page_list(set_name)
+    card_list = []
+    for page in page_url
+        card_url = get_detail_url_list(page)
+        card_list.concat(card_url)
+    end
+    flavor_list = []
+    for detail_url in card_list
+        oracle_doc = get_html_doc(detail_url)
+        print_doc = get_html_doc(detail_url)
+        card_data = parse_card(oracle_doc, print_doc)
+        flavor_list.push(card_data)
+    end
+    CSV.open("#{set_name}_flavor.csv", "wb") do |file|
+        file << flavor_list
+    end
+end
+
+if __FILE__ == $0
+    get_flavour_file($1)
 end
 
