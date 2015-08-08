c0dc2af701d06d63cba30673ea35d2a35b882f1c
wataraimu <wataraimu@gmail.com>
2015-08-08 20:22:07 +0900

    構文エラーを修正

--- gatherer/gatherer_url.rb
+++ gatherer/gatherer_url.rb
@@ -1,22 +1,23 @@
 
 require "uri"
 
-def is_round_dquote(str):
+def is_round_dquote(str)
     return str.length >= 2 and str[0] == '"' and str[-1] =='"'
 end
 
-def round_dquote(str):
+def round_dquote(str)
     strip = str.strip()
-    if not is_round_dquote(strip):
+    if not is_round_dquote(strip) then
         strip = '"' + strip + '"'
+    end
     return strip
 end
 
-def card_set_url(set, page=0):
+def card_set_url(set, page=0)
     return "http://gatherer.wizards.com/Pages/Search/Default.aspx?page=#{page}&set=[#{uri.escape(round_dquote(set))}]"
 end
 
-def card_detail_url(name, id, printed=True):
+def card_detail_url(name, id, printed=True)
     return "http://gatherer.wizards.com/Pages/Card/Details.aspx?printed=#{printed}&multiverseid=#{id}&part=#{uri.escape(name)}"
 end
 
