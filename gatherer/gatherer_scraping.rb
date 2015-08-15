1e91417f4da757b994b45b08ee490fd2a14ca1f7
wataraimu <wataraimu@gmail.com>
2015-08-15 23:10:20 +0900

    リファクタリング URL取得処理の切り出し

--- gatherer/gatherer_scraping.rb
+++ gatherer/gatherer_scraping.rb
@@ -7,6 +7,25 @@ require "../utils/nokogiri_utils"
 
 def get_flavor_file(set_name)
     # フレーバーテキスト一覧を取得する。
+    card_list = get_card_url_list(set_name)
+    rng = Random.new
+    flavor_list = []
+    for card_path in card_list
+        detail_url = get_gatherer_page_url(card_path)
+        oracle_doc = get_html_doc(detail_url)
+        print_doc = get_html_doc(detail_url)
+        card_data = parse_card(oracle_doc, print_doc)
+        output_data = [card_data[:en][:name], card_data[:ja][:name], card_data[:ja][:flavor]]
+        flavor_list.push(output_data)
+        sleep 1+rng.rand
+    end
+    CSV.open("#{set_name}_flavor.csv", "wb") do |file|
+        file << flavor_list
+    end
+end
+
+def get_card_url_list(set_name)
+    # 指定したカードセットのURLリストを取得する。
     search_url = card_set_url(set_name)
     # print(search_url)
     search_doc = get_html_doc(search_url)
@@ -18,20 +37,7 @@ def get_flavor_file(set_name)
         card_sub_list = get_detail_url_list(page_doc)
         card_list.concat(card_sub_list)
     end
-    rng = Random.new
-    flavor_list = []
-    for card_path in card_list
-        detail_url = get_gatherer_page_url(card_path)
-        oracle_doc = get_html_doc(detail_url)
-        print_doc = get_html_doc(detail_url)
-        card_data = parse_card(oracle_doc, print_doc)
-        output_data = [card_data[:en][:name], card_data[:ja][:name], card_data[:ja][:flavor]]
-        flavor_list.push(output_data)
-        sleep rng.rand
-    end
-    CSV.open("#{set_name}_flavor.csv", "wb") do |file|
-        file << flavor_list
-    end
+    return card_list
 end
 
 if __FILE__ == $0
