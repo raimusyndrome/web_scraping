f37e7cd6f1f11d2cf3bd60ccbce6db6b43ec5330
wataraimu <wataraimu@gmail.com>
2016-02-26 00:53:42 +0900

    処理の定義を整理

--- gatherer/gatherer_crawling.rb
+++ gatherer/gatherer_crawling.rb
@@ -2,7 +2,8 @@
 # -*- encoding: utf-8 -*-
 
 require "csv"
-require "./gatherer_parser"
+require "./gatherer_page_parser"
+require "./gatherer_card_parser"
 require "./gatherer_url"
 require "../utils/nokogiri_utils"
 
@@ -11,7 +12,7 @@ DEBUG=true
 def crawl_set_page(set_name)
     # 指定したカードセットのページをファイルに取得する。
     output_dir = "result"
-    set_dir = File.join(output_dir, set_name.gsub("\s", ""))
+    set_dir = File.join(output_dir, set_name.gsub("\s", "_"))
     if !Dir.exist?(set_dir)
         Dir.mkdir(set_dir)
     end
@@ -21,6 +22,8 @@ def crawl_set_page(set_name)
         card_list = card_list[0..1]
     end
     cookie = { 'CardDatabaseSettings' => '1=ja-JP' }
+    oracle_dir = File.join(set_dir, 'oracle')
+    print_dir = File.join(set_dir, 'print')
     for card_path in card_list
         if /multiverseid=(\d+)/ =~ card_path
             multiverseid = $1
@@ -28,8 +31,8 @@ def crawl_set_page(set_name)
             print_file = "card_#{multiverseid}_print.html"
             detail_url = get_gatherer_page_url(card_path)
             # print(detail_url)
-            download_html(detail_url, out_dir=set_dir, file_name=oracle_file, cookie=cookie)
-            download_html(printed_url(detail_url), out_dir=set_dir, file_name=print_file, cookie=cookie)
+            download_html(detail_url, out_dir=oracle_dir, file_name=oracle_file, cookie=cookie)
+            download_html(printed_url(detail_url), out_dir=print_dir, file_name=print_file, cookie=cookie)
             sleep 1+rng.rand
         else
             puts "unexpected URL: #{card_path}"
